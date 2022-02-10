c90da3038cfb1af04cccf2243740fa0a30c11bbc
MOHAMEDYOSRAAAAAAAY <99403737+MOHAMEDYOSRAAAAAAAY@users.noreply.github.com>
2022-02-10 14:22:54 +0200

    Delete UART_CODE directory

--- UART_CODE/Debug/UART/UART_Program.d
+++
@@ -1,8 +0,0 @@
-UART/UART_Program.o UART/UART_Program.o: ../UART/UART_Program.c \
-  ../UART/UART_Privet.h ../UART/UART_Interface.h ../UART/UART_Confeg.h
-
-../UART/UART_Privet.h:
-
-../UART/UART_Interface.h:
-
-../UART/UART_Confeg.h:
